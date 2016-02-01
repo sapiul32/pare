e8dbc95323c7b6cbaa7b8b75c7d48dbfef2fd84a
sapiul <sapiulgemu@gmail.com>
2016-02-01 14:22:10 +0700

    Create newbie.cpp

---
+++ newbie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//constanta area
+#define space 50
+//main area
+using namespace std;
+
+int main(){
+ if(space < 60){
+  cout << "show in frame say hello world";
+  return 0;
+ }
+}
